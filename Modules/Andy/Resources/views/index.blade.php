8f4f2bbeeabd11d31e28fa51a7ff3af42177b943
guoyang <Guo_Yang_0@163.com>
2019-04-26 17:44:49 +0800

    first commit

---
+++ Modules/Andy/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('andy::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('andy.name') !!}
+    </p>
+@stop
---
+++ app/Http/Controllers/administrator/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers\administrator;
+
+
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+
+}
